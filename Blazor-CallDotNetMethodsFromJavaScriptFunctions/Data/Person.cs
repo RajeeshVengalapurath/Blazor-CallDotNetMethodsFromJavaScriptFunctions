ddb4cd94eee678ae85618b5f6b328b9afba62df6
Rajeesh VP <rajeeshvp@gmail.com>
2021-02-20 12:01:31 +0530

    Instance method call

---
+++ Blazor-CallDotNetMethodsFromJavaScriptFunctions/Data/Person.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_CallDotNetMethodsFromJavaScriptFunctions.Data
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public Person()
+        {
+            Name = "Rajeesh";
+        }
+        [JSInvokable]
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
